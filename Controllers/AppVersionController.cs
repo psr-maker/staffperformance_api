85b4e9bb43a6e5c66477c73e43a6a67b6e865253
psr-maker <randdpoornasreekeerthana@gmail.com>
2026-09-01 16:10:22 +0530

    Version

---
+++ Controllers/AppVersionController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace staff.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppVersionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetVersion()
+        {
+            return Ok(new
+            {
+                latestVersion = "1.0.0",
+                downloadUrl = "https://staff.poornasreecloud.com/downloads/workpulse.apk",
+                forceUpdate = false,
+                message = "A new version of WorkPulse is available."
+            });
+        }
+    }
+}
