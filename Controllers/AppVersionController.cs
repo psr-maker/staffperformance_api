2c7e990b877ea76519c3d3ec196ef3b5078ba3d8
psr-maker <randdpoornasreekeerthana@gmail.com>
2026-09-12 11:59:43 +0530

    ccc

--- Controllers/AppVersionController.cs
+++ Controllers/AppVersionController.cs
@@ -11,7 +11,7 @@ namespace staff.Controllers
         {
             return Ok(new
             {
-                latestVersion = "1.0.2",
+                latestVersion = "1.0.2+4",
                 downloadUrl = "https://staff.poornasreecloud.com/downloads/workpulse.apk",
                 forceUpdate = false,
                 message = "A new version of WorkPulse is available."
